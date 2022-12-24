99ee470ccb3c644095a3672cfd1749a73949add2
osennyaya <supersduby@gmail.com>
2022-12-25 03:49:44 +0800

    github action

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 CARGO_TARGET_DIR=wasm/ cargo build --release --target wasm32-unknown-unknown
+
+# Optimize for size.
+wasm-opt -Os -o quadtris.wasm wasm/wasm32-unknown-unknown/release/quadtris.wasm
+cp -r res web/
